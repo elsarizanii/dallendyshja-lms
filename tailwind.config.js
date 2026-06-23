cf718bc9971ce36cf9384435291de858458dfd76
Elsa Rizani <elsaa.rizanii@gmail.com>
2026-06-23 18:06:39 +0200

    Configuration of Flowbite and Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
+        "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
@@ -20,5 +21,8 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms, 
+        require('flowbite/plugin')
+    ],
 };
